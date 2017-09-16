25146e99e294bfe424b382dc7ea70a3f927bf69e
smatsumori <shoya.matsumori@gmail.com>
2017-09-16 16:23:06 +0900

    WIP: add tf.FeatureExtractor

--- agent/config/model.py
+++ agent/config/model.py
@@ -4,5 +4,6 @@ from . import BASE_DIR
 CNN_FEATURE_EXTRACTOR = BASE_DIR + '/model/alexnet_feature_extractor.pickle'
 CAFFE_MODEL = BASE_DIR + '/model/bvlc_alexnet.caffemodel'
 MODEL_TYPE = 'alexnet'
+TF_CNN_FEATURE_EXTRACTOR = BASE_DIR + '/tfalex/mynet.npy'
 
 DEFAULT_MEAN_IMAGE = BASE_DIR + '/model/ilsvrc_2012_mean.npy'
