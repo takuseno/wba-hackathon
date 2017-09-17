e1e54a63b747a7b6b30f169757716998e4da8933
smatsumori <shoya.matsumori@gmail.com>
2017-09-17 14:23:08 +0900

    MOD: tffetch to wget

--- tffetch.sh
+++ tffetch.sh
@@ -2,4 +2,4 @@
 
 echo "downloading tf-alex..."
 dir=./agent/tfalex
-curl -o ${dir}/mynet.npy https://www.dropbox.com/s/caz3uunv267ogks/tf_alexnet.npy
+wget -O ${dir}/mynet.npy https://www.dropbox.com/s/caz3uunv267ogks/tf_alexnet.npy
